91b2e77e939577f16453cb643cbb3baafac0b3d0
Aleksi Vesanto <avjves@gmail.com>
2021-01-17 21:47:57 +0000

    Fix query

--- frontend/static/frontend/scripts.js
+++ frontend/static/frontend/scripts.js
@@ -3,20 +3,39 @@ function searchq() {
 	const currentUrl = window.location.href;
 	const getParams = window.location.search.substr(1)
 	const splitParams = getParams.split("&");
+	var qfound = false;
 	var newParams = [];
 	for(var i = 0; i < splitParams.length; i++) {
 		key = splitParams[i].split("=")[0]
+		if(key.length == 0) {
+			continue;
+		}
 		console.log(key);
 		value = splitParams[i].substring(key.length + 1)
 		if(key == "q") {
 			value = q;
+			qfound = true;
 		}
 		if(key == 'fq') {
 			continue;
 		}
 		newParams.push(key + "=" + value);
-
 	}
-	window.location = currentUrl.replace(getParams, newParams.join("&"));
+	if(!qfound) {
+		newParams.push("q=" + q);
+	}
+	console.log(currentUrl)
+	console.log(getParams)
+	console.log(newParams)
+	var newUrl = '';
+	if(getParams.length == 0) {
+		newUrl = currentUrl + "?" + newParams.join("&");	
+		console.log(newUrl);
+	}
+	else {
+		newUrl = currentUrl.replace(getParams, newParams.join("&"));
+	}
+	console.log(newUrl);
+	window.location = newUrl;
 }
 
