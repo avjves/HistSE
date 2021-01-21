1f9e04c933d5478926f5569abc113a4c3a32b4c0
Aleksi Vesanto <avjves@gmail.com>
2021-01-21 18:26:20 +0000

    Add highlighting to templates

--- frontend/templatetags/template_methods.py
+++ frontend/templatetags/template_methods.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 
 register = template.Library()
@@ -10,3 +11,19 @@ def get_item(dictionary, key):
 @register.filter
 def get_index(l, index):
     return l[index-1]
+
+@register.filter
+def show_em(data, key):
+    if key == 'text' or key == 'first_text':
+        splits = data.split("<em>")
+        fixed_data = [splits.pop(0)]
+        for splitt in splits:
+            fixed_data.append(mark_safe('<em class="no-whitespace">'))
+            second_splits = splitt.split("</em>")
+            fixed_data.append(second_splits.pop(0))
+            fixed_data.append(mark_safe("</em>"))
+        if splits:
+            fixed_data.append(second_splits.pop(0))
+        return fixed_data
+    else:
+        return [data]
